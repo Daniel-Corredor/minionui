4d66fc8f3d964394bf8be8fa6e741a674a4fad36
Daniel Corredor <Daniel.corredor@inchcape.com>
2024-05-24 20:30:56 -0500

    add dts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { resolve } from "path";
-
+import dts from "vite-plugin-dts";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dts({ include: ["lib"] })],
   build: {
     copyPublicDir: false,
     lib: {
